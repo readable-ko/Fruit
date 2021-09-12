d035c67c4d83317c56a3d11332d0f14638ae29bd
readable-ko <21700026@handong.edu>
2021-09-12 09:49:57 +0900

    updated

--- src/edu/handong/csee/lab2/fruit/Fruit.java
+++ src/edu/handong/csee/lab2/fruit/Fruit.java
@@ -1,32 +1,10 @@
 package edu.handong.csee.lab2.fruit;
 
+import java.util.Comparator;
+
 public class Fruit {
 	
 	int no;
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
 	String name;
 	int price;
 	
@@ -39,4 +17,25 @@ public class Fruit {
 	public String toString() {
 		return no + " - " + name + "(" + price + ")";
 	}
+	
+}
+
+class FruitComparator implements Comparator<Fruit>{
+
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		// TODO Auto-generated method stub
+		return o1.name.compareTo(o2.name);
+	}
+	
+}
+
+class FruitComparatorDesc implements Comparator<Fruit> {
+
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		// TODO Auto-generated method stub
+		return o2.name.compareTo(o1.name);
+	}
+
 }
